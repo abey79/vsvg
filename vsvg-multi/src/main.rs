4998b2f553a5210ac7549fafc92bda005ba50046
Antoine Beyeler <abeyeler@ab-ware.com>
2023-04-29 10:59:48 +0200

    vsvg-multi: added placeholder UI code

--- vsvg-multi/src/main.rs
+++ vsvg-multi/src/main.rs
@@ -2,6 +2,37 @@
 #![allow(clippy::module_name_repetitions)]
 #![allow(clippy::missing_errors_doc)]
 
-fn main() {
-    println!("Hello, world!");
+mod multi_viewer;
+
+use crate::multi_viewer::MultiViewer;
+use std::error::Error;
+use std::path::PathBuf;
+
+fn show(paths: Vec<PathBuf>) -> Result<(), Box<dyn Error>> {
+    let native_options = eframe::NativeOptions::default();
+
+    eframe::run_native(
+        "vsvg multi",
+        native_options,
+        Box::new(move |cc| {
+            let style = egui::Style {
+                visuals: egui::Visuals::light(),
+                ..egui::Style::default()
+            };
+            cc.egui_ctx.set_style(style);
+
+            Box::new(MultiViewer::new(paths))
+        }),
+    )?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let paths = std::env::args()
+        .skip(1)
+        .map(PathBuf::from)
+        .collect::<Vec<_>>();
+
+    show(paths)
 }
---
+++ vsvg-multi/src/multi_viewer.rs
@@ -0,0 +1,43 @@
+use egui::Color32;
+use std::path::PathBuf;
+
+pub(crate) struct MultiViewer {
+    paths: Vec<PathBuf>,
+    selected_path: usize,
+}
+
+impl MultiViewer {
+    pub(crate) fn new(paths: Vec<PathBuf>) -> Self {
+        Self {
+            paths,
+            selected_path: 0,
+        }
+    }
+}
+
+impl eframe::App for MultiViewer {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // handle input
+        if !ctx.wants_keyboard_input() {}
+
+        egui::SidePanel::left("left_panel").show(ctx, |ui| {
+            ui.input(|i| {
+                if i.key_pressed(egui::Key::ArrowUp) {
+                    self.selected_path = self.selected_path.saturating_sub(1);
+                } else if i.key_pressed(egui::Key::ArrowDown) {
+                    self.selected_path = (self.selected_path + 1).min(self.paths.len() - 1);
+                }
+            });
+            for (i, path) in self.paths.iter().enumerate() {
+                ui.selectable_value(&mut self.selected_path, i, path.to_string_lossy());
+            }
+        });
+
+        let panel_frame = egui::Frame::central_panel(&ctx.style())
+            .inner_margin(egui::style::Margin::same(0.))
+            .fill(Color32::from_rgb(242, 242, 242));
+        egui::CentralPanel::default()
+            .frame(panel_frame)
+            .show(ctx, |ui| ui.label("hello world"));
+    }
+}
