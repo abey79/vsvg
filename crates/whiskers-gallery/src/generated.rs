6cc2d26232a8e1a188ab0e6be0334c6fd08d032f
Antoine Beyeler <abeyeler@ab-ware.com>
2025-12-31 23:05:11 +0100

    change sketch order

--- crates/whiskers-gallery/src/generated.rs
+++ crates/whiskers-gallery/src/generated.rs
@@ -6,15 +6,15 @@ use crate::sketches::SketchMeta;
 /// Registry of all available sketches.
 pub static SKETCH_MANIFEST: &[SketchMeta] = &[
     SketchMeta {
-        id: "schotter",
-        name: "Schotter",
-        description: "Recreation of Georg Nees' classic 1968-1970 generative art piece",
-        author: "Antoine Beyeler",
-    },
-    SketchMeta {
         id: "hello_world",
         name: "Hello World",
         description: "A simple introductory sketch demonstrating basic whiskers usage",
+        author: "Antoine Beyeler",
+    },
+    SketchMeta {
+        id: "schotter",
+        name: "Schotter",
+        description: "Recreation of Georg Nees' classic 1968-1970 generative art piece",
         author: "Antoine Beyeler",
     },
 ];
@@ -23,8 +23,8 @@ pub static SKETCH_MANIFEST: &[SketchMeta] = &[
 #[cfg(not(target_arch = "wasm32"))]
 pub fn render_sketch(id: &str, seed: u32) -> Option<anyhow::Result<vsvg::Document>> {
     match id {
-        "schotter" => Some(crate::sketches::schotter::runner().run_headless(seed)),
         "hello_world" => Some(crate::sketches::hello_world::runner().run_headless(seed)),
+        "schotter" => Some(crate::sketches::schotter::runner().run_headless(seed)),
         _ => None,
     }
 }
@@ -34,22 +34,22 @@ mod wasm {
     use wasm_bindgen::prelude::*;
 
     #[wasm_bindgen]
-    pub async fn start_schotter(
-        handle: &vsvg_viewer::web_handle::WebHandle,
-        canvas: vsvg_viewer::exports::web_sys::HtmlCanvasElement,
-    ) -> Result<(), JsValue> {
-        handle
-            .start(canvas, crate::sketches::schotter::runner())
-            .await
-    }
-
-    #[wasm_bindgen]
     pub async fn start_hello_world(
         handle: &vsvg_viewer::web_handle::WebHandle,
         canvas: vsvg_viewer::exports::web_sys::HtmlCanvasElement,
     ) -> Result<(), JsValue> {
         handle
             .start(canvas, crate::sketches::hello_world::runner())
+            .await
+    }
+
+    #[wasm_bindgen]
+    pub async fn start_schotter(
+        handle: &vsvg_viewer::web_handle::WebHandle,
+        canvas: vsvg_viewer::exports::web_sys::HtmlCanvasElement,
+    ) -> Result<(), JsValue> {
+        handle
+            .start(canvas, crate::sketches::schotter::runner())
             .await
     }
 }
